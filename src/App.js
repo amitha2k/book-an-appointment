ef569ea7239ae95d462f72df6b105326250dae1c
Amitha <amitha2k@gmail.com>
2023-03-03 01:54:00 -0500

    Changed table head and fixed next and prev button bugs

--- src/App.js
+++ src/App.js
@@ -2,66 +2,82 @@ import React, { useState } from 'react';
 import './style.css';
 
 function Calendar() {
+  //Today's Date:
   const [selectedDate, setSelectedDate] = useState(new Date());
 
+  //Constants for day, month and time names
   const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   const hoursOfDay = Array.from(Array(9), (_, i) => i + 9);
   const halfHoursOfDay = hoursOfDay.flatMap(hour => [`${hour}:00`, `${hour}:30`]);
 
-  const previousMonth = () => {
-    setSelectedDate(new Date(selectedDate.getFullYear(), selectedDate.getMonth() - 1, 1));
+  //previousWeek and nextWeek mutate selectedDate to the date 7 days prior and later respectively.
+  const previousWeek = () => {
+    setSelectedDate(new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate() - 7));
   };
 
-  const nextMonth = () => {
-    setSelectedDate(new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, 1));
+  const nextWeek = () => {
+    setSelectedDate(new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate() + 7));
   };
 
+  //monthName consumes a number from 0-11 and returns the corresponding month name.
   function monthName(monthNum) {
-    return months[monthNum - 1];
-  }
-  const daysInMonth = () => {
-    const month = selectedDate.getMonth();
-    const year = selectedDate.getFullYear();
-    const daysInMonth = new Date(year, month + 1, 0).getDate();
-    const daysInPrevMonth = new Date(year, month, 0).getDate();
-    const firstDayOfMonth = new Date(year, month, 1).getDay();
-    const lastDayOfMonth = new Date(year, month, daysInMonth).getDay();
-    const days = [];
-    for (let i = firstDayOfMonth - 1; i >= 0; i--) {
-      days.push(daysInPrevMonth - i);
-    }
-    for (let i = 1; i <= daysInMonth; i++) {
-      days.push(i);
-    }
-    for (let i = 1; i <= 6 - lastDayOfMonth; i++) {
-      days.push(i);
-    }
-    return days;
+    return months[monthNum];
   };
+
+  const getDaysOfWeekForCurrentWeek = () => {
+    const currentWeekStart = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate()-selectedDate.getDay());
+    const currentWeekEnd = new Date(currentWeekStart.getFullYear(), currentWeekStart.getMonth(), currentWeekStart.getDate() + 6);
+    const nextWeekStart = new Date(currentWeekEnd.getFullYear(), currentWeekEnd.getMonth(), currentWeekEnd.getDate() + 1);
+    const nextWeekEnd = new Date(nextWeekStart.getFullYear(), nextWeekStart.getMonth(), nextWeekStart.getDate() + 6);
+    return [currentWeekStart, currentWeekEnd, nextWeekStart, nextWeekEnd];
+  };
+
+  const [currentWeekStart, currentWeekEnd, nextWeekStart, nextWeekEnd] = getDaysOfWeekForCurrentWeek();
+
+  const getWeekDates = () => {
+    const dates = [];
+    for (let i = 0; i < 7; i++) {
+      dates.push(new Date(currentWeekStart.getFullYear(), currentWeekStart.getMonth(), currentWeekStart.getDate()+i));
+    }
+    return dates;
+  } 
+
+  const datesOfWeek = getWeekDates();
+
+  const datesAndDays = [];
+  for (let i = 0; i < 7; i++) {
+    datesAndDays.push([datesOfWeek[i], daysOfWeek[i]]);
+  }
 
   return (
     <div className="calendar-container">
-      <h1 class="header">Week of {monthName(selectedDate.getMonth())} {selectedDate.getDay()} {selectedDate.getFullYear()}</h1>
-      <button class = "header" onClick={previousMonth}>&lt;</button>
-      <button class = "header" onClick={nextMonth}>&gt;</button>
+      <h1 className="header">Week of {currentWeekStart.getDate()} {monthName(currentWeekStart.getMonth())} {currentWeekStart.getFullYear()}</h1>
+      <button onClick={previousWeek}>&lt;</button>
+      <button onClick={nextWeek}>&gt;</button>
+      <hl></hl>
       <table className="calendar">
         <thead>
           <tr>
             <th></th>
-            {daysOfWeek.map((day) => (
-              <th key={day}>{day}</th>
-            ))}
+            {datesAndDays.map((pair) => (
+              <th key={pair[1]}><span className="dates">{pair[0].getDate()}</span><br></br><span className="days">{pair[1]}</span></th>
+              ))}
           </tr>
         </thead>
         <tbody>
           {halfHoursOfDay.map((hour) => (
             <tr key={hour}>
               <td>{hour}</td>
-              {daysOfWeek.map((day) => (
-                <td key={`${day}-${hour}`}></td>
-              ))}
+              {daysOfWeek.map((day) => {
+                const currentDate = new Date(`${currentWeekStart.getMonth()+1}/${currentWeekStart.getDate()}/${currentWeekStart.getFullYear()}`);
+                const cellDate = new Date(`${currentDate.getMonth()+1}/${currentDate.getDate()+daysOfWeek.indexOf(day)}/${currentDate.getFullYear()}`);
+                if (cellDate >= currentWeekStart && cellDate <= nextWeekEnd) {
+                  return <td key={`${day}-${hour}`}></td>;
+                }
+                return <td key={`${day}-${hour}`} className="inactive"></td>;
+              })}
             </tr>
           ))}
         </tbody>
@@ -71,4 +87,3 @@ function Calendar() {
 }
 
 export default Calendar;
-
